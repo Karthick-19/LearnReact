7e4b83cb7057d0821543a9daf5c0f885c6f48b80
Karthick-19 <kcruze19@gmail.com>
2024-02-26 21:47:34 +0530

    Added Controlled Inputs

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { FaPlus } from "react-icons/fa";
 
-const AddItem = () => {
+const AddItem = ({newItem,SetnewItem,handleSubmit}) => {
     return (
-        <form className="addForm">
+        <form className="addForm" onSubmit={(e) => handleSubmit(e)}>
             <label htmlFor="addItem">Add Item</label>
             <input 
+            id="addItem"
             type="text"
             autoFocus
-            id="addItem"
             placeholder="Add Item"
+            value={newItem}
+            onChange={(e) => SetnewItem(e.target.value)}
             required
             ></input>
             <button type="submit" aria-label="Add Item">
--- src/App.js
+++ src/App.js
@@ -7,30 +7,55 @@ import AddItem from './AddItem';
 
 
 function App() {
-  const [item,setItems] = useState(
-    [{id:1,checked:true,item:"React Coding"},{id:2,checked:false,item:"Angular Coding"},{id:3,checked:true,item:"Spring Coding"}]);
-const handleChange = (id) => {
-    const listItems = item.map((item) => 
+  const [items,setItems] = useState(
+    [{id:1,checked:true,item:"React Coding"},
+    {id:2,checked:false,item:"Angular Coding"},
+    {id:3,checked:true,item:"Spring Coding"}]);
+
+  const handleChange = (id) => {
+    const listItems = items.map((item) => 
         item.id === id ? {...item,checked:!item.checked} : item
     )
     setItems(listItems)
-}
-const handleDelete = (id) => {
-    const delItems = item.filter((item) => item.id !== id)
+  }
+
+  const handleDelete = (id) => {
+    const delItems = items.filter((item) => item.id !== id)
     setItems(delItems)
     // console.log('Delete Works')
+}
+
+const [newItem,SetnewItem] = useState('')
+
+const addItem = (item) => {
+  const id = items.length ? items[items.length - 1].id + 1 : 1;
+  const addnewItem = {id,checked:false,item};
+  const listItems = [...items,addnewItem];
+  setItems(listItems);
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  // console.log(newItem)
+  if (!newItem) return;
+  addItem(newItem)
+  SetnewItem('')
 }
   return (
     <div className='App'>
       <Header/>
-      <AddItem/>
+      <AddItem
+      newItem = {newItem}
+      SetnewItem = {SetnewItem}
+      handleSubmit = {handleSubmit}
+      />
       <TodoContent 
-      item = {item}
+      item = {items}
       handleChange = {handleChange}
       handleDelete = {handleDelete}
       />
       <Footer
-      length = {item.length}
+      length = {items.length}
       />
     </div> 
     
